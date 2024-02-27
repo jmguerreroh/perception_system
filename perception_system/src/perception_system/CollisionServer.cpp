ad371966f33b77bdf7cfc79c885ed40b57bb698b
juan <juan97pena@gmail.com>
2024-02-27 18:24:49 +0100

    collision server made component

--- perception_system/src/perception_system/CollisionServer.cpp
+++ perception_system/src/perception_system/CollisionServer.cpp
@@ -510,3 +510,10 @@ void CollisionServer::isolate_pc_background_service_callback(
 
 
 }  // namespace perception_system
+
+#include "rclcpp_components/register_node_macro.hpp"
+
+// Register the component with class_loader.
+// This acts as a sort of entry point, allowing the component to be discoverable when its library
+// is being loaded into a running process.
+RCLCPP_COMPONENTS_REGISTER_NODE(perception_system::CollisionServer)
